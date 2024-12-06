de7047bb3c84e2b666f281fb0506ee5dc106ba39
south <fancustard9@gmail.com>
2024-12-06 16:07:37 +0900

    いらない部分消した

--- src/newpage-button.tsx
+++ src/newpage-button.tsx
@@ -1,31 +1,6 @@
 import React from "react";
 import "antd/dist/reset.css";
 
-// const contentStyle: React.CSSProperties = {
-//   height: "160px",
-//   color: "#fff",
-//   lineHeight: "160px",
-//   textAlign: "center",
-//   background: "#364d79",
-// };
-
-// export const App: React.FC = () => (
-//   <Carousel autoplay={true}>
-//     <div>
-//       <h3 style={contentStyle}>1</h3>
-//     </div>
-//     <div>
-//       <h3 style={contentStyle}>2</h3>
-//     </div>
-//     <div>
-//       <h3 style={contentStyle}>3</h3>
-//     </div>
-//     <div>
-//       <h3 style={contentStyle}>4</h3>
-//     </div>
-//   </Carousel>
-//);
-
 export const NewpageButton = () => {
   return <button style={{ width: "700px" }}>さあ！お買い物をしよう！</button>;
 };
