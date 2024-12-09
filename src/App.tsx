62390dbf79606a2abcaadf04d9d061d9d10d5bea
south <fancustard9@gmail.com>
2024-12-09 13:38:19 +0900

    なぜ、import React from reactはいるときといらないときがあるのか

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Scroll from "./scroll-auto";
 import { NewpageButton } from "./newpage-button";
