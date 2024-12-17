e80c22a1fd332b8a22ea53676ebd6d8f051dfd86
south <fancustard9@gmail.com>
2024-12-17 11:23:01 +0900

    一旦ログインまでもって行けた

--- src/login.tsx
+++ src/login.tsx
@@ -1,11 +1,18 @@
 import AuthComponent from "./AuthComponent";
+import { User } from "firebase/auth"; // User 型をインポート
 import "./login.css";
 
 const Login = () => {
+  // ユーザー変更時のコールバック関数 (User | null 型を指定)
+  const handleUserChange = (user: User | null) => {
+    console.log("ログイン状態が変更されました:", user);
+  };
+
   return (
     <div>
       <h1>ログイン</h1>
-      <AuthComponent />
+      {/* AuthComponent に onUserChange を渡す */}
+      <AuthComponent onUserChange={handleUserChange} />
     </div>
   );
 };
