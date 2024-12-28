2e7f7e52eb75c3dbb7ee60715ddf18d042a442ff
south <fancustard9@gmail.com>
2024-12-28 15:18:54 +0900

    ちょい直し

--- src/sell.tsx
+++ src/sell.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { storage } from "../firebase";
 import { ref, listAll, getDownloadURL } from "firebase/storage";
 import { saveCaptionToFirestore } from "./save"; // インポート
